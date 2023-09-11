8b6512dfddeca7a4eca2d90c437de63cf0064541
youpiteron <oyushchenko@kse.org.ua>
2023-09-11 13:21:37 +0300

    Assignment 2

--- Script.js
+++ Script.js
@@ -1,1 +1,11 @@
-// JavaScript source code
+/*function addData (){
+let name = document.getElementById("name");
+let question = document.getElementById("question");
+
+let table = document.getElementById("outputTable");
+let newRow = table.insertRow(table.rows.length);
+
+newRow.insertCell(0).innerHTML = name;
+newRow.insertCell(1).innerHTML = question;
+clearInputs();
+}*/
